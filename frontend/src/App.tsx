c8b9d7db78759a5c5514b8b810bfc0a7c66fb389
Paulo <paulo_sanches2012@hotmail.com>
2021-01-07 13:17:53 -0300

    Add Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 import './GlobalStyles.css';
 
 function App() {
   return (
+    <>
       <Navbar/>
+      <Home/>
+    </>
   );
 }
 
